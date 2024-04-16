41874b7826bc1ebb87d9fa3d5a596255dd213936
Kendall Schmit <kendall504@me.com>
2024-04-15 23:38:19 -0700

    Add screenshots to trigger fullscreen dimming

--- misc/32gs95ue_test_patterns/lrtest.js
+++ misc/32gs95ue_test_patterns/lrtest.js
@@ -1,6 +1,8 @@
+var spaceimage_src = 'a.png'
+
 function reset() {
 
-    const images = document.querySelectorAll('.testimage')
+    const images = document.querySelectorAll('.testimage');
     images.forEach((image) => {
         image.addEventListener('click', (e) => {
             if (document.fullscreenElement) {
@@ -10,5 +12,24 @@ function reset() {
                 image.requestFullscreen();
             }
         })
+    });
+
+
+    const image = document.getElementById('spaceimage').src = spaceimage_src;
+
+    document.addEventListener('keyup', (event) => {
+        if (event.code === 'Space') {
+            if (spaceimage_src == 'a.png') {
+                spaceimage_src = 'b.png';
+            }
+            else {
+                spaceimage_src = 'a.png';
+            }
+
+            const image = document.getElementById('spaceimage').src = spaceimage_src;
+        }
     })
+
+
+
 }
